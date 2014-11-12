48df592fbaba56471da30d966ec927faf90928d0
ThemePixels <themepixels@gmail.com>
2014-11-13 03:37:32 +0800

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      dev: {
+        files: {
+          '<%= pkg.name %>.css' : '<%= pkg.name %>.less'
+        }
+      },
+
+      prod: {
+        options: {
+          compress: true
+        },
+        files: {
+          '<%= pkg.name %>.min.css' : '<%= pkg.name %>.less'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['<%= pkg.name %>.less'],
+        tasks: ['less']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000
+        }
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['less', 'connect', 'watch']);
+
+
+};
